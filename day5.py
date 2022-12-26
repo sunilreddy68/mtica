5688e35877a1deb0f3e99a61c2a3bc6e344a31b1
sunilreddy68 <121158457+sunilreddy68@users.noreply.github.com>
2022-12-26 16:54:09 +0530

    Add files via upload

---
+++ day5.py
@@ -0,0 +1,13 @@
+def remove_duplicate(s):
+    str2=' '
+    for i in s:
+        if i not in str2:
+            str2+=i
+            #print(str2)
+    return str2
+
+
+
+str1=input("enter you text: ")
+print ("without duplicate:",remove_duplicate(str1))
+ 
