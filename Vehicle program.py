730d538bfa9cb76b1f65d268c8723304111ccdf5
Sunil Reddy <121158457+sunilreddy68@users.noreply.github.com>
2023-01-08 15:58:48 +0530

    Add files via upload

---
+++ Vehicle program.py
@@ -0,0 +1,6 @@
+class Vehicle:
+    def __init__(self,max_speed,mileage):
+        self.max_speed=max_speed
+        self.mileage=mileage
+modelS=Vehicle(240,18)
+print(modelS.max_speed,modelS.mileage)
---
+++ day15program5.py
@@ -0,0 +1,17 @@
+def findGCD(n1,n2):
+    if n1<0 or n2<0:
+        return "INVALID"
+    if n1>n2:
+        n1,n2=n2,n1
+    i=n2
+    while True:
+        if i%n1==0 and i%n2==0:
+            return i
+        else:
+            i+=1
+    return None
+print("enter two numbers")
+a = int(input())
+b = int(input())
+
+print(findLCM(a,b))
